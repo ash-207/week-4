03f6305c62ffb9cb57fc097fed988ef9305be5fa
vikramdev772 <vikramdev772@gmail.com>
2026-01-07 14:34:01 +0530

    auth - signup

--- myapp/src/main/java/com/example/myapp/controller/Authentication.java
+++ myapp/src/main/java/com/example/myapp/controller/Authentication.java
@@ -1,5 +1,6 @@
 package com.example.myapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,15 +8,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.myapp.dto.SignupRequest;
+import com.example.myapp.model.User;
+import com.example.myapp.repo.UserRepository;
 
 @RestController
 @RequestMapping("/api")
 public class Authentication {
+
+    @Autowired
+    UserRepository   db;  
+
+
     @PostMapping("/signup")
     String m(@RequestBody SignupRequest obj){
         System.out.println("\t name :"+obj.getName());
         System.out.println("\t name :"+obj.getEmail());
         System.out.println("\t name :"+obj.getPassword());
+
+        User u=new User();
+
+        u.setName(obj.getName());
+        u.setEmail(obj.getEmail());
+        u.setPassword(obj.getPassword());
+
+        db.save(u);
+
         return "signup sucess";
     }
 }
