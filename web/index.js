7339d41e2e1af7c3b3d13f1e298167e553964202
vikramdev772 <vikramdev772@gmail.com>
2026-01-28 11:35:04 +0530

    web

---
+++ web/index.js
@@ -0,0 +1,34 @@
+
+let signupObject={
+    name:"test",
+    email:"test@gmail.com",
+    password:"12345"
+}
+
+console.log(" signup data : "+signupObject);
+console.log(" signup data : "+JSON.stringify(signupObject));
+console.log("\n\t JSON OBJECT \n");
+console.log(" signup data : "+JSON.stringify(signupObject,null,2));
+
+let api="http://40.0.20.123:4040/api/signup";
+
+async  function signup(){
+    let n=document.getElementById("n").value;
+    let e=document.getElementById("e").value;
+    let p=document.getElementById("p").value;
+    let obj={
+        name:n,
+        email:e,
+        password:p,
+    }
+    console.log(" data : "+JSON.stringify(obj,null,2));
+    let res=await fetch(api,{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(obj)
+    })
+    let data=await res.text();
+    console.log(" api response : "+data);
+}
